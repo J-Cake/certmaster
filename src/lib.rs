685c7da4018acd805323c5b41ba493ed1a64cc3f
jcake <jakieschneider13@gmail.com>
2025-09-30 23:45:41 +0200

    I decided to just reindex the inbox on modification because we'd be more likely to find files we otherwise missed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 mod convert;
+mod debounce;
 
+use std::alloc::System;
 use std::path::PathBuf;
+use std::time::SystemTime;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Config {
     pub redis: RedisConfig,
 
@@ -11,19 +14,30 @@ pub struct Config {
     pub runner: RunnerConfig
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct RedisConfig {
     pub url: String,
     pub channel: Option<String>,
     pub db: Option<u32>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct ReceiverConfig {
     pub inbox: PathBuf,
+    
+    pub rescan_interval: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct RunnerConfig {
     pub outbox: PathBuf,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub received: SystemTime,
+    pub path: String,
+    pub csr: String,
+}
+
+pub use debounce::debounce;
--- src/receiver.rs
+++ src/receiver.rs
@@ -1,7 +1,16 @@
+use std::mem::forget;
 use clap::Parser;
-use config::Config;
+use config::{debounce, Config};
+use config::Task;
 use notify::Watcher;
+use redis::AsyncTypedCommands;
+use std::path::Path;
 use std::path::PathBuf;
+use std::sync::Arc;
+use std::sync::OnceLock;
+use std::time::Duration;
+use std::time::SystemTime;
+use tokio::sync::mpsc;
 
 /// # Receiver
 /// The receiver awaits directory changes and issues tasks according to the name of the item in the inbox.
@@ -12,24 +21,75 @@ pub struct Args {
     config: PathBuf,
 }
 
+static CONFIG: OnceLock<Arc<Config>> = OnceLock::new();
+
 #[tokio::main]
 pub async fn main() {
     env_logger::init();
 
     log::info!("Starting receiver");
 
+    let config = read_config().await;
+
+    let (req_tx, req_rx) = mpsc::channel(100);
+    let (fs_tx, fs_rx) = mpsc::unbounded_channel();
+
+    let (reindex_tx, reindex_rx) = mpsc::channel(100);
+
+    let senders = (req_tx.clone(), fs_tx.clone(), reindex_tx.clone());
+
+    let mut watcher = init_watcher(fs_tx).await;
+
+    let _req_tx = req_tx.clone();
+    let __req_tx = req_tx.clone();
+    let initial = tokio::spawn(async move { read_inbox(_req_tx.clone()).await });
+    let fs_events = tokio::spawn(async move { handle_events(fs_rx, reindex_tx).await });
+    let dispatcher = tokio::spawn(async move { dispatch_to_redis(req_rx).await });
+    let reindex = tokio::spawn(async move { watch_reindex(reindex_rx, __req_tx).await });
+
+    watcher
+        .watch(config.receiver.inbox.as_path(), notify::RecursiveMode::Recursive)
+        .expect("Failed to start watcher");
+
+    let (..) = tokio::join!(initial, fs_events, dispatcher, reindex);
+
+    // Keep references alive because dropping them will cause the watcher to stop.
+    drop(senders.clone());
+    drop(watcher);
+}
+
+async fn read_config() -> Arc<Config> {
     let args = Args::parse();
 
     let config = tokio::fs::read_to_string(args.config)
         .await
         .expect("Failed to read config file");
-    let config = toml::from_str::<Config>(&config).expect("Failed to parse config file");
 
-    let redis = redis::Client::open(config.redis.url).expect("Failed to connect to redis");
+    let config = Arc::new(toml::from_str::<Config>(&config).expect("Failed to parse config file"));
+
+    CONFIG.set(config.clone()).expect("Failed to set config");
+
+    return config;
+}
+
+async fn init_watcher(fs_tx: mpsc::UnboundedSender<notify::Event>) -> impl Watcher {
+    let config = CONFIG.get().cloned().unwrap_or_default();
 
     tokio::fs::create_dir_all(config.receiver.inbox.as_path())
         .await
         .expect("Failed to create inbox");
+
+    notify::recommended_watcher(move |event: notify::Result<notify::Event>| {
+        let event = event.expect("Failed to get event");
+        let tx = fs_tx.clone();
+
+        tx.send(event).expect("Failed to send event");
+    })
+    .expect("Failed to watch inbox")
+}
+
+async fn read_inbox(sender: mpsc::Sender<PathBuf>) {
+    let config = CONFIG.get().cloned().unwrap_or_default();
 
     let mut dir = tokio::fs::read_dir(config.receiver.inbox.as_path())
         .await
@@ -37,29 +97,68 @@ pub async fn main() {
 
     while let Some(entry) = dir.next_entry().await.expect("Failed to read inbox") {
         if entry.file_type().await.expect("Failed to stat file").is_file() && entry.path().extension().is_some_and(|ext| ext == "csr") {
-            log::trace!("Found unprocessed request: {path:?}", path = entry.path());
+            sender.send(entry.path()).await.expect("Failed to send request");
+        }
+    }
+}
+
+async fn watch_reindex(rx: mpsc::Receiver<()>, sender: mpsc::Sender<PathBuf>) {
+    let config = CONFIG.get().cloned().unwrap_or_default();
+    let mut rx = debounce(rx, Duration::from_secs(config.receiver.rescan_interval));
+    while let Some(_) = rx.recv().await {
+        log::trace!("Reindexing");
+
+        let sender = sender.clone();
+        tokio::spawn(async move {
+            read_inbox(sender.clone()).await;
+        });
+    }
+}
+
+async fn handle_events(mut rx: mpsc::UnboundedReceiver<notify::Event>, reindex: mpsc::Sender<()>) {
+    while let Some(event) = rx.recv().await {
+        match &event.kind {
+            notify::EventKind::Create(_) | notify::EventKind::Modify(_) => if let Err(_) = reindex.send(()).await {},
+
+            _ => {}
         }
     }
 
-    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+    log::trace!("No more events");
+}
 
-    let _tx = tx.clone();
-    let mut watcher = notify::recommended_watcher(move |event: notify::Result<notify::Event>| {
-        let event = event.expect("Failed to get event");
-        let tx = tx.clone();
+async fn dispatch_to_redis(mut rx: mpsc::Receiver<PathBuf>) {
+    let config = CONFIG.get().cloned().unwrap_or_default();
 
-        tx.send(event)
-            .expect("Failed to send event");
-    })
-    .expect("Failed to watch inbox");
+    let mut redis = redis::Client::open(config.redis.url.as_ref())
+        .expect("Failed to connect to redis")
+        .get_multiplexed_async_connection()
+        .await
+        .expect("Failed to connect to redis");
 
-    watcher
-        .watch(config.receiver.inbox.as_path(), notify::RecursiveMode::Recursive)
-        .expect("Failed to start watcher");
+    while let Some(path) = rx.recv().await {
+        log::trace!("Dispatching request: {path:?}");
 
-    while let Some(event) = rx.recv().await {
+        let Some(str) = path.to_str() else {
+            log::warn!("Invalid request path: Contains non-UTF-8 characters - Skipping {path:?}");
+            continue;
+        };
 
+        let now = SystemTime::now();
+        let contents = tokio::fs::read_to_string(&path).await.expect("Failed to read request");
+
+        let payload = ron::to_string(&Task {
+            received: now,
+            path: str.to_owned(),
+            csr: contents,
+        })
+        .expect("Failed to serialize task");
+
+        redis.rpush("task_queue", payload)
+            .await.expect("Failed to dispatch request");
+
+        tokio::fs::remove_file(&path).await.expect("Failed to remove request");
     }
 
-    drop(_tx.clone());
+    log::trace!("Nothing more to dispatch");
 }
